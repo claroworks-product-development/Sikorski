319d177b0b2e6be89a383e65cb84c8bfb8d51e81
Mike Wilson <mail4mikew@gmail.com>
2021-07-23 18:35:53 -0700

    adjust motor max temperature settings for testing

--- mcconf_412_Sikorski.h
+++ mcconf_412_Sikorski.h
@@ -67,10 +67,10 @@
 #define MCCONF_L_LIM_TEMP_FET_END 100
 
 // Motor Temp Cutoff Start
-#define MCCONF_L_LIM_TEMP_MOTOR_START 155
+#define MCCONF_L_LIM_TEMP_MOTOR_START 140
 
 // Motor Temp Cutoff End
-#define MCCONF_L_LIM_TEMP_MOTOR_END 200
+#define MCCONF_L_LIM_TEMP_MOTOR_END 155
 
 // Acceleration Temperature Decrease
 #define MCCONF_L_LIM_TEMP_ACCEL_DEC 0.15
